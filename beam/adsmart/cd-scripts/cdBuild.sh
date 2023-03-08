682489f415d4ec43c6ac58fd7f7ecff62836efe0
Vlad Kravets <vlad.kravets@nbcuni.com>
2023-03-08 10:04:30 -0500

    testing

--- beam/adsmart/cd-scripts/cdBuild.sh
+++ beam/adsmart/cd-scripts/cdBuild.sh
@@ -1,7 +1,8 @@
 #BASEDIR=$(dirname "$0")
-echo 'current dir is $(pwd)'
+echo "current dir is $(pwd)"
 #task_script="${BASEDIR}/${DIRECTORY}/cd-scripts/${TASK}.sh"
 #echo "Path to the script is ${task_script}"
 pushd ${DIRECTORY}
-    echo 'now, current dir is $(pwd)'
+    echo "now, current dir is $(pwd)"
+    ls
 popd 
