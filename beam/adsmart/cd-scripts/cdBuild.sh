76411e42f698f180cee214e0ae0a52f4ebf98f22
Vlad Kravets <vlad.kravets@nbcuni.com>
2023-03-08 10:03:01 -0500

    testing scripts

--- beam/adsmart/cd-scripts/cdBuild.sh
+++ beam/adsmart/cd-scripts/cdBuild.sh
@@ -1,4 +1,7 @@
-BASEDIR=$(dirname "$0")
-
-task_script="${BASEDIR}/${DIRECTORY}/cd-scripts/${TASK}.sh"
-echo "Path to the script is ${task_script}"
+#BASEDIR=$(dirname "$0")
+echo 'current dir is $(pwd)'
+#task_script="${BASEDIR}/${DIRECTORY}/cd-scripts/${TASK}.sh"
+#echo "Path to the script is ${task_script}"
+pushd ${DIRECTORY}
+    echo 'now, current dir is $(pwd)'
+popd 
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,5 @@ BASEDIR="$(dirname "$0")/.."
 SCRIPT="${DIRECTORY}/cd-scripts/${TASK}.sh"
 
 pushd $BASEDIR
-#chmod +x $SCRIPT
     bash $SCRIPT
 popd
